1d6710092968f2834ad426f5da96981edebedf7b
DennisSchmitz <48991783+DennisSchmitz@users.noreply.github.com>
2024-07-24 17:29:19 +0200

    fix: intel conda channel no longer exists, replaced

--- Jovian/__init__.py
+++ Jovian/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 __package_name__ = "Jovian"
 __package_dir__ = os.path.dirname(os.path.abspath(__file__))
 __home_env_configuration__ = os.path.join(os.path.expanduser("~"), ".jovian_env.yaml")
